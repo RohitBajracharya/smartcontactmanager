a5500914a3a3f1f796bf7023310e3caf89e07154
RohitBajra <104704874+rohitnewgit@users.noreply.github.com>
2023-06-24 20:16:41 +0545

    Pagination added

--- smartcontactmanager/src/main/java/com/smart/controller/UserController.java
+++ smartcontactmanager/src/main/java/com/smart/controller/UserController.java
@@ -6,20 +6,24 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.Principal;
-import java.util.List;
+
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
 
 import com.smart.dao.ContactRepository;
 import com.smart.dao.UserRepository;
@@ -99,8 +103,10 @@ public class UserController {
 	
 	
 //	handler for open view contact
-	@GetMapping("/show-contacts")
-	public String showContacts(Model model,Principal principal) {
+//	per page = 5[n]
+//	current page= 0[page]
+	@GetMapping("/show-contacts/{page}")
+	public String showContacts(@PathVariable("page") Integer page,Model model,Principal principal) {
 		model.addAttribute("title","Show Contact");
 //	 alternative
 		/*
@@ -110,8 +116,12 @@ public class UserController {
 		 */
 		String userName = principal.getName();
 		User user = this.userRepository.getUserByUserName(userName);
-		List<Contact> contacts = this.contactRepository.findContactsByUser(user.getId());
+//		current page, contact per page
+		org.springframework.data.domain.Pageable pageable = PageRequest.of(page, 5);
+		Page<Contact> contacts = this.contactRepository.findContactsByUser(user.getId(),pageable);
 		model.addAttribute("contacts",contacts);
+		model.addAttribute("currentPage",page);
+		model.addAttribute("totalPages",contacts.getTotalPages());
 		return "normal/show_contacts";
 	}
 }
--- smartcontactmanager/src/main/java/com/smart/dao/ContactRepository.java
+++ smartcontactmanager/src/main/java/com/smart/dao/ContactRepository.java
@@ -1,7 +1,8 @@
 package com.smart.dao;
 
-import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,5 +13,5 @@ public interface ContactRepository extends JpaRepository<Contact, Integer> {
 //	method for pagination
 	
 	@Query("from Contact as c where c.user.id =:userId")
-	public List<Contact> findContactsByUser(@Param("userId")int userId);
+	public Page<Contact> findContactsByUser(@Param("userId")int userId,Pageable pageable);
 }
